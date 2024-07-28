68d913d43d3fd209a6cc68315e97de76067a6116
Open Cipher <8209379+open-cipher@users.noreply.github.com>
2024-07-28 08:08:03 +0530

    fix(nest-probot): update folder reference

--- packages/nest-probot/jest.config.ts
+++ packages/nest-probot/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/nest-probot',
+  coverageDirectory: '../../coverage/packages/nest-probot',
 };
